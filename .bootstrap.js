11f4c3da5cabb9c3a714b02b82804cc86455d590
Vibesdk <vibesdk-bot@cloudflare.com>
2026-01-12 04:08:03 +0000

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun install uuidv7"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
